f0da5ec681674ab6906073a2a07a8039490f77a3
Andrew Newman <andrewfnewman@gmail.com>
2013-01-24 20:25:49 +1000

    Tests for open.

---
+++ spec/lib/oo/command/open_command_spec.rb
@@ -0,0 +1,55 @@
+require File.dirname(__FILE__) + "/../../../../lib/oo/haunted_house"
+
+describe 'open command' do
+  context "study" do
+    before { @house = OO::HauntedHouse.new(43) }
+
+    specify "should have correct description" do
+      @house.current_room.description.should eql("Study with a Desk and Hole in the Wall")
+    end
+
+    context "open desk" do
+      before { open("desk") }
+      specify { @message.should eql("Drawer open.") }
+      specify { @house.current_room.objects.should =~ [Inventory::CANDLE] }
+    end
+
+    context "open drawer" do
+      before { open("drawer") }
+      specify { @message.should eql("Drawer open.") }
+      specify { @house.current_room.objects.should =~ [Inventory::CANDLE] }
+    end
+  end
+
+  context "study" do
+    before { @house = OO::HauntedHouse.new(28) }
+
+    specify "should have correct description" do
+      @house.current_room.description.should eql("Hall by a Thick Wooden Door")
+    end
+
+    context "open door" do
+      before { open("door") }
+      specify { @message.should eql("It's locked.") }
+    end
+  end
+
+  context "cellar with coffin" do
+    before { @house = OO::HauntedHouse.new(38) }
+
+    specify "should have correct description" do
+      @house.current_room.description.should eql("Deep Cellar with a Coffin")
+    end
+
+    context "open coffin" do
+      before { open("coffin") }
+      specify { @message.should eql("That's creepy!") }
+      specify { @house.current_room.objects.should =~ [Inventory::RING] }
+    end
+  end
+
+  def open(object)
+    parser = Parser.new(@house)
+    @message = parser.parse_input("open #{object}")
+  end
+end
